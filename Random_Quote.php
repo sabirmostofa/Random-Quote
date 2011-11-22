fb81c35fb6f10681c3832f4cd65924dd1baefac1
gypsy <s@gypsy-pc>
2011-11-22 18:26:25 +0600

    finalizing

--- Random_Quote.php
+++ Random_Quote.php
@@ -1,13 +1,32 @@
 <?php
-
 /*
-  Plugin Name: WP-Random-Quote
-  Plugin URI: http://sabirul-mostofa.blogspot.com
-  Description: Display Random quotes in your sidebar or in any page using shortcode
+  Plugin Name: WP Random Quote
+  Plugin URI: http://www.qotd.org
+  Description: Display a random quote provided by QOTD.org in your sidebar as a widget or in a page/post using  shortcode. For more info:<a href="http://www.qotd.org/wp-plugin.html">www.qotd.org/wp-plugin.html</a>
   Version: 1.0
   Author: Sabirul Mostofa
-  Author URI: http://sabirul-mostofa.blogspot.com
+  Author URI: http//:sabirul-mostofa.blogspot.com
+  License: GPLv2
  */
+
+
+/*
+  Copyright 2011 Sabirul Mostofa  (email : sabirmostofa@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 include_once 'widget.php';
 if (!class_exists('Random_Quote')):
 
@@ -17,6 +36,8 @@ if (!class_exists('Random_Quote')):
             //initializing widget
             add_action('widgets_init', create_function('', 'register_widget("Random_Quote_Widget");'));
             add_action('wprq_cron_hook', array($this, 'cron_func'));
+            add_shortcode('random-quote', array($this, 'random_quote_shortcode'));
+            add_action('admin_menu', array($this, 'CreateMenu'));
         }
 
         function cron_func() {
@@ -31,7 +52,7 @@ if (!class_exists('Random_Quote')):
                 $html = wp_remote_get('http://wpquote.qotd.org/');
                 if (isset($html['body'])):
                     preg_match('/<body>(.*)<\/body>/s', $html['body'], $matches);
-                    $quote = trim($matches[0]);
+                    $quote = trim($matches[1]);
                     if (!in_array($quote, $quotes) && !in_array($quote, $prev)) {
                         $quotes[] = $quote;
                         if (++$count == 10)
@@ -49,6 +70,40 @@ if (!class_exists('Random_Quote')):
                 wp_clear_scheduled_hook('wprq_cron_hook');
                 wp_schedule_event(time(), $sched, 'wprq_cron_hook');
             }
+        }
+
+        // shortcode function
+        function random_quote_shortcode($atts= array()) {
+            extract(shortcode_atts(array(
+                        'font' => '12px'
+                            ), $atts));
+            $quotes = get_option('wprq_random_quotes');
+            if (!$quotes)
+                $quotes = $this->cron_func();
+            $random_quote = $quotes[rand(0, count($quotes))];
+
+            $r_quote = "<div  style=\"font-size: $font\"> $random_quote</div>";
+            return $r_quote;
+        }
+
+        //adding menu under Setting main menu
+        function CreateMenu() {
+            add_options_page('Random Quote', 'Random Quote', 'activate_plugins', 'wpRandomQuote', array($this, 'OptionPage'));
+        }
+
+        function OptionPage() {
+            ?>
+            <div class ="wrap">
+                <h3>Available shortcodes for the Random Quote plugin</h3>
+                In any post or page add the shotcode <b>[random-quote]</b>    <br/>
+
+                Default font size is 12px. To change the font size add the shortcode like this 
+                <b> [random-quote font="16px"] </b> <br/>
+
+                You can find the random quote widget from the widgets section
+
+            </div>
+            <?php
         }
 
     }
--- widget.php
+++ widget.php
@@ -11,8 +11,9 @@ class Random_Quote_Widget extends WP_Widget {
     function form($instance) {
         // outputs the options form on admin
         $defs = array(
-            'title' => '',
-            'interval' => 'hourly'
+            'title' => 'A Quote from quotd.org',
+            'interval' => 'hourly',
+            'font-size'=>'12px'
         );
         $instance = wp_parse_args((array) $instance, $defs);
         ?>
@@ -21,13 +22,16 @@ class Random_Quote_Widget extends WP_Widget {
             <input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($instance['title']); ?>" class="widefat" /></p>
 
         <!-- Check Interval -->
-        <p><label for="<?php echo $this->get_field_id('interval'); ?>"><?php echo 'Interval between fetching the new quotes(In minutes)'; ?>:</label>
+        <p><label for="<?php echo $this->get_field_id('interval'); ?>"><?php echo 'Interval between fetching the new quotes'; ?>:</label>
             <select  id="<?php echo $this->get_field_id('interval'); ?>" name="<?php echo $this->get_field_name('interval'); ?>" >
                 <option <?php selected($instance['interval'], 'hourly'); ?> value="hourly">Hourly</option>
                 <option <?php selected($instance['interval'], 'twicedaily'); ?> value="twicedaily" >Twice Daily</option>
                 <option <?php selected($instance['interval'], 'daily'); ?> value="daily" >Daily</option>
 
             </select>
+         <!-- Font size -->
+                 <p><label for="<?php echo $this->get_field_id('font-size'); ?>"><?php echo 'Font Size(Default: 12px)'; ?>:</label>
+            <input type="text" id="<?php echo $this->get_field_id('font-size'); ?>" name="<?php echo $this->get_field_name('font-size'); ?>" value="<?php echo esc_attr($instance['font-size']); ?>" class="widefat" /></p>
 
             <?php
         }
@@ -38,22 +42,30 @@ class Random_Quote_Widget extends WP_Widget {
                 global $random_quote;
                 $random_quote->add_change_cron($new_instance['interval']);
             }
-       
+
             return $new_instance;
         }
 
         function widget($args, $instance) {
-            global $random_quote;
             // outputs the content of the widget
-            $quotes = get_option('wprq_random_quotes');
-            if(!$quotes)
-                $quotes = $random_quote ->cron_func();
-            $random_quote = $quotes[rand(0, count($quotes))];
-            ?>
-             <div id="wp_random_quote"><?php echo $random_quote ?></div>
-            <?php
-        }
+            extract($args);
+            global $random_quote;
+            
+            
+            echo $before_widget;
 
+            if (!empty($instance['title']))
+                echo $before_title . apply_filters('widget_title', $instance['title']) . $after_title;
+
+            $quotes = get_option('wprq_random_quotes');
+            if (!$quotes)
+                $quotes = $random_quote->cron_func();
+            $random_quote = $quotes[rand(0, (count($quotes)-1  ))];
+            ?>
+        <div id="wp_random_quote" style="font-size:<?php echo $instance['font-size'] ?>"><?php echo $random_quote ?></div>
+        <?php
+        echo $after_widget;
     }
 
-    
+}
+
